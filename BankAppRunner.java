7a427b5b9dad396ac3cece7e0c352cc20ef9a23f
Amiel <str8lanes@gmail.com>
2026-07-27 14:57:23 -0400

    Changed HashMap
    users and admins

--- BankAppRunner.java
+++ BankAppRunner.java
@@ -1,46 +1,89 @@
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class BankAppRunner {
-   static Scanner sc;
-   static Map<String, String> map;
+   static Scanner sc = new Scanner(System.in);
+   static Map<String, User> map = new HashMap<>();
 
    public BankAppRunner() {
    }
 
-   public static void main(String[] var0) {
-      printMessage("Welcome to our bank");
-      String var1 = mylogin();
-      printMessage("Welcome " + var1);
-   }
+   public static void main(String[] logincred) {
+      System.out.println("Welcome to our bank");
 
-   private static void printMessage(String var0) {
-      System.out.println(var0);
-   }
+      boolean overallLoopFlag =true;
+      while(overallLoopFlag){
+        System.out.println("Do you want to login or not? Y/N");
 
-   private static String mylogin() {
-      System.out.println("Please enter username and password separated by space");
-      String var0 = sc.nextLine();
-      String[] var1 = var0.split(" ");
-      String var2 = var1[0];
-      String var3 = var1[1];
-      if (map.containsKey(var2) && ((String)map.get(var2)).equals(var3)) {
-         System.out.println("Login successful");
-         return var2;
-      } else {
-         System.out.println("Invalid username or password");
-         return null;
+        String choice = sc.nextLine();
+        if(choice.equalsIgnoreCase("Y")){
+          redirect();
+          overallLoopFlag = false;
+        }else if(choice.equalsIgnoreCase("N")){
+            overallLoopFlag = false;
+            System.out.println("Thank you for using our bank application. Goodbye!");
+        }else{
+            System.out.println("Invalid choice. Please enter Y or N.");
+        }
       }
+
+
+      // printMessage("Welcome " + mylogin());
    }
 
+   // redirect console user to either admin or customer page based on their login credentials
+   private static void redirect() {
+      System.out.println("Redirecting to the appropriate page...");
+        User user = mylogin();
+        if (user != null) {
+            if (user.getUsername().equals("admin")) {
+                adminPage();
+            } else {
+                customerPage();
+            }
+        }
+   }
+
+   // loginss --------------------------------
+    private static User mylogin() {
+        System.out.println("Please enter username and password separated by space");
+        String inputLogin = sc.nextLine();
+        String[] logincred = inputLogin.split(" ");
+        String username = logincred[0];
+        String password = logincred[1];
+
+        User user = (User) map.get(username);
+        if(user !=null && user.getPassword().equals(password)) {
+            System.out.println("Login successful");
+            return user;
+        } else {
+            System.out.println("Invalid username or password");
+            return null;
+        }
+        
+    }
+
+    // admin page
+   private static void adminPage() {
+      System.out.println("Welcome to the admin page");
+      
+   }
+
+   // customer page
+   private static void customerPage() {
+      System.out.println("Welcome to the customer page");
+   }
+
+   
+
+   
+
+   
+   
    static {
-      sc = new Scanner(System.in);
-      map = new HashMap();
-      map.put("admin", "admin123");
-      map.put("user2", "pass2");
-      map.put("user1", "pass1");
+      map.put("admin", new User("admin", "admin123"));
+      map.put("user1", new User("user1", "pass1"));
+      map.put("user2", new User("user2", "pass2"));
+      map.put("amiel", new User("amiel", "halili"));
    }
 }
 
@@ -51,9 +94,9 @@ class User {
    private String username;
    private String password;
 
-   public User(String var1, String var2) {
-      this.username = var1;
-      this.password = var2;
+   public User(String username, String password) {
+      this.username = username;
+      this.password = password;
    }
 
    public String getUsername() {
@@ -67,28 +110,56 @@ class User {
 
 //class admin extrends user
 class Admin extends User {
-   public Admin(String var1, String var2) {
-      super(var1, var2);
-   }
-
-   public void addUser(String var1, String var2) {
-      BankAppRunner.map.put(var1, var2);
-   }
-
-   public void removeUser(String var1) {
-      BankAppRunner.map.remove(var1);
+   public Admin(String username, String password) {
+      super(username, password);
    }
 }
 //class customer extends user
 class Customer extends User {
-   public Customer(String var1, String var2) {
-      super(var1, var2);
+   public Customer(String username, String password) {
+      super(username, password);
    }
 }
 
-//abstract classa ccount
+
+
+//abstract class Account
+abstract class Account {
+   private String accountNumber;
+   private double balance;
+
+   public Account(String accountNumber, double balance) {
+      this.accountNumber = accountNumber;
+      this.balance = balance;
+   }
+
+   private String getAccountNumber() {
+      return this.accountNumber;
+   }
+
+   private double getBalance() {
+      return this.balance;
+   }
+
+   private void setBalance(double balance) {
+      this.balance = balance;
+   }
+}
 //checkingsaccount extends account
+class CheckingAccount extends Account {
+    public CheckingAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+    }
+
 //savings account extrewnds account
+class SavingsAccount extends Account {
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+}
+
+
 
 //interfact AccountOperations: printInterestRate(), deposit, withdraw, transfer.
 //SavingsAccount always gives higher itnerst rate
