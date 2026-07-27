8395efc2ee16267299eb89ff2af5eac5be19a2da
Amiel <str8lanes@gmail.com>
2026-07-27 13:54:00 -0400

    added user, admin, and normal customer

--- BankAppRunner.java
+++ BankAppRunner.java
@@ -43,3 +43,52 @@ public class BankAppRunner {
       map.put("user1", "pass1");
    }
 }
+
+
+
+//class user
+class User {
+   private String username;
+   private String password;
+
+   public User(String var1, String var2) {
+      this.username = var1;
+      this.password = var2;
+   }
+
+   public String getUsername() {
+      return this.username;
+   }
+
+   public String getPassword() {
+      return this.password;
+   }
+}
+
+//class admin extrends user
+class Admin extends User {
+   public Admin(String var1, String var2) {
+      super(var1, var2);
+   }
+
+   public void addUser(String var1, String var2) {
+      BankAppRunner.map.put(var1, var2);
+   }
+
+   public void removeUser(String var1) {
+      BankAppRunner.map.remove(var1);
+   }
+}
+//class customer extends user
+class Customer extends User {
+   public Customer(String var1, String var2) {
+      super(var1, var2);
+   }
+}
+
+//abstract classa ccount
+//checkingsaccount extends account
+//savings account extrewnds account
+
+//interfact AccountOperations: printInterestRate(), deposit, withdraw, transfer.
+//SavingsAccount always gives higher itnerst rate
