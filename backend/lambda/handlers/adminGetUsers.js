45914a8b87301e0e9a887a3acd936b1d31508aee
str8lanes@gmail.com <str8lanes@gmail.com>
2026-07-31 09:54:37 -0400

    remove lmabda folder, dead code

--- backend/lambda/handlers/adminGetUsers.js
+++
@@ -1,9 +0,0 @@
-// GET /bank/admin/users
-import * as bankService from '../../services/bankService.js';
-import { success } from '../lib/http.js';
-import { withHandler } from '../lib/withHandler.js';
-
-export const handler = withHandler(async () => {
-  const users = await bankService.getAllUsernames();
-  return success({ users });
-}, { errorStatus: 500 });
